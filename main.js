f461cc0243edbb927019abf5e65ed1e2dc159dd8
Vladimir <example@gmail.com>
2022-12-08 13:38:11 +0200

    test work

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const userName = "John";
-const result = userName * 9;
-console.log(Boolean(result)); 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последниз просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последниз просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+
+console.log(personalMovieDB);
